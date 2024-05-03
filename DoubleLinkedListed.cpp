e2494c32cbba7b1f15fc436cf682a464f0416d47
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-03 14:36:40 +0700

    commit9

--- DoubleLinkedListed.cpp
+++ DoubleLinkedListed.cpp
@@ -76,6 +76,23 @@ bool search (int rollNo, Node **previous, Node **current)
     *current = START;
     while (*current != NULL && (*current)->noMhs != rollNo)
     {
-        
+        *previous = *current;
+        *current = (*current)->next;
     }
+    return (*current != NULL);
+}
+
+void deleteNode()
+{
+    Node *previous, *current; //
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo; // step 3 : get the roll number to be deleted 
+
+    if (START == NULL)
+    {
+        cout << "List is empty" << endl;
+        return;
+    } 
 }
