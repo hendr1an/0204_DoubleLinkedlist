b1dcd5d8984d85271436b82731a0e96b8ad1006f
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-03 14:49:24 +0700

    commit10

--- DoubleLinkedListed.cpp
+++ DoubleLinkedListed.cpp
@@ -94,5 +94,31 @@ void deleteNode()
     {
         cout << "List is empty" << endl;
         return;
+    }
+
+    current = START; // step 1 : start from the first node 
+    previous = NULL;
+
+    // Locate the node to be deleted 
+    while (current != NULL && current->noMhs != rollNo)
+    {
+        previous = current;
+        current = current->next;
     } 
+
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+        return;
+    }
+
+    // Node to be deleted is the first node
+    if (current == START)
+    {
+        START = START->next; // step 2 : update the START pointer
+        if (START != NULL)
+        {
+            START->prev = NULL;
+        }
+    }
 }
